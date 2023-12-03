6805db8aa092b32855e8057bfb5fae6e6cf22d8f
Mohamed Abdel Ghany <mufasa1611@gmail.com>
2023-12-03 16:44:20 +0000

    Entering player name

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
 # Import Required Module 
 import random
-
+        
 # Define the Initial Board
 rows = [[1, 2, 3, 4, 5, 6, 7],
         [1, 2, 3, 4, 5],
         [1, 2, 3]]
 
-        
+#  Get Player Name
+def get_player_name():
+    while True:
+        player_name = input("Enter your name: ").strip()
+        if player_name:
+            return player_name        
