62ed7e27d9d19d5a31e5cfcd47aa1f7da3d54ad7
Mohamed Abdel Ghany <mufasa1611@gmail.com>
2023-12-04 21:19:37 +0000

    Setup player selections

--- run.py
+++ run.py
@@ -9,9 +9,9 @@ rows = [[1, 2, 3, 4, 5, 6, 7],
 #  Get Player Name
 def get_player_name():
     while True:
-        player_name = input("Enter your name: ").strip()
-        if player_name:
-            return player_name      
+        player = input("Enter your name: ").strip()
+        if player:
+            return player      
 
 # Display the Initial Board
 def display_board(board):
@@ -28,3 +28,26 @@ def take_units(row, units):
     row[-units:] = []
     return True
     
+# Execute Player Turn    
+def player_turn(board, player):
+    print(f"\n{player}'s Turn:")
+    display_board(board)
+
+# Select a row
+    selected_row = None
+    while selected_row is None or selected_row < 1 or selected_row > 3 or len(board[selected_row - 1]) == 0:
+        selected_row = int(input("Choose a row (1, 2, or 3): "))
+        if selected_row < 1 or selected_row > 3:
+            print("Invalid row choice. Please enter 1, 2, or 3.")
+        elif len(board[selected_row - 1]) == 0:
+            print("That row is empty. Choose another row.")
+    selected_row = board[selected_row - 1]
+
+# Select units
+    units = None
+    while units is None or units < 1 or units > len(selected_row):
+        units = int(input(f"How many units to take from row {selected_row}? "))
+        if units < 1 or units > len(selected_row):
+            print(f"Invalid input. Please enter a number between 1 and {len(selected_row)}.")
+
+    take_units(selected_row, units)
