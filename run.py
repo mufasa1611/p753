5d8f6b62edc2ac9f08f0725a6e0bd8a492ffd1f2
Mohamed Abdel Ghany <mufasa1611@gmail.com>
2023-12-04 22:25:20 +0000

    change name of variables

--- run.py
+++ run.py
@@ -2,9 +2,9 @@
 import random
         
 # Define the Initial Board
-rows = [[1, 2, 3, 4, 5, 6, 7],
-        [1, 2, 3, 4, 5],
-        [1, 2, 3]]
+Initial_board = [[1, 2, 3, 4, 5, 6, 7],
+                [1, 2, 3, 4, 5],
+                [1, 2, 3]]
 
 #  Get Player Name
 def get_player_name():
@@ -33,21 +33,25 @@ def player_turn(board, player):
     print(f"\n{player}'s Turn:")
     display_board(board)
 
-# Select a row
-    selected_row = None
-    while selected_row is None or selected_row < 1 or selected_row > 3 or len(board[selected_row - 1]) == 0:
-        selected_row = int(input("Choose a row (1, 2, or 3): "))
-        if selected_row < 1 or selected_row > 3:
+    # Select a row
+    pick_row = None
+    while pick_row is None or pick_row < 1 or pick_row > 3 or len(board[pick_row - 1]) == 0:
+        pick_row = int(input("Choose a row (1, 2, or 3): "))
+        if pick_row < 1 or pick_row > 3:
             print("Invalid row choice. Please enter 1, 2, or 3.")
-        elif len(board[selected_row - 1]) == 0:
+        elif len(board[pick_row - 1]) == 0:
             print("That row is empty. Choose another row.")
-    selected_row = board[selected_row - 1]
 
-# Select units
+    selected_row = board[pick_row - 1]
+
+    # Select units
     units = None
     while units is None or units < 1 or units > len(selected_row):
-        units = int(input(f"How many units to take from row {selected_row}? "))
+        units = int(input(f"How many units to take from row {pick_row}? "))
         if units < 1 or units > len(selected_row):
             print(f"Invalid input. Please enter a number between 1 and {len(selected_row)}.")
 
     take_units(selected_row, units)
+
+
+    
