2a601edbd7db950624a33064b1ae63fd01fe10a6
Mohamed Abdel Ghany <mufasa1611@gmail.com>
2023-12-05 22:27:17 +0000

    re-format code

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-# Import Required Module 
+# Import Required Module
 import random
 import copy
 
@@ -12,31 +12,34 @@ def get_player_name():
     while True:
         player = input("Enter your name: ").strip()
         if player:
-            return player      
+            return player
 
 # Display the Initial Board
 def display_board(board):
     for i, row in enumerate(board, start=1):
-         formatted_row = f"row {i}: " + " ".join([str(unit) for unit in row])
-         print(formatted_row)
+        formatted_row = f"row {i}: " + " ".join([str(unit) for unit in row])
+        print(formatted_row)
 
 # Take Units from a row
 def take_units(row, units):
     if units < 1 or units > len(row):
-        print(f"Invalid input. You can take between 1 and {len(row)} items. Please Try again.")
+        print(
+            f"Invalid input. You can take between 1 \
+            and {len(row)} items. Please Try again.")
         return False
 
     row[-units:] = []
     return True
-    
-# Execute Player Turn    
+
+# Execute Player Turn
 def player_turn(board, player):
     print(f"\n{player}'s Turn:")
     display_board(board)
 
     # Select a row
     pick_row = None
-    while pick_row is None or pick_row < 1 or pick_row > 3 or len(board[pick_row - 1]) == 0:
+    while pick_row is None or pick_row < 1 or \
+            pick_row > 3 or len(board[pick_row - 1]) == 0:
         pick_row = int(input("Choose a row (1, 2, or 3): "))
         if pick_row < 1 or pick_row > 3:
             print("Invalid row choice. Please enter 1, 2, or 3.")
@@ -50,14 +53,15 @@ def player_turn(board, player):
     while units is None or units < 1 or units > len(selected_row):
         units = int(input(f"How many units to take from row {pick_row}? "))
         if units < 1 or units > len(selected_row):
-            print(f"Invalid input. Please enter a number between 1 and {len(selected_row)}.")
+            print(f"Invalid input. Please enter a number \
+                between 1 and {len(selected_row)}.")
 
     take_units(selected_row, units)
 
     # Computer turn
 def computer_turn(board):
     print("\nComputer's Turn:")
-    
+
     available_rows = [i for i, row in enumerate(board) if len(row) > 0]
     if not available_rows:
         print("Computer wins!")
@@ -88,11 +92,13 @@ def play_game(player):
             print("Computer wins!")
             break
 
-# Game intro 
+
+# Game intro
 print("\nWelcome to the Game of Rows!")
 print("In this game, you have three rows of tokens (represented by numbers).")
 print("Each row has a different number of tokens.")
-print("On your turn, you can choose a row and take a certain number of tokens from it.")
+print("On your turn, you can choose a row and \
+take a certain number of tokens from it.")
 print("The goal is to leave the last unit for your opponent.\n")
 print("Let's get started!\n")
 
@@ -101,9 +107,9 @@ player = get_player_name()
 while True:
     play_game(player)
 
-# Retry or quit option   
-    play_again = input(f"Do you want to play again {player}? (yes/no): ").strip().lower()
+# Retry or quit option
+    play_again = input(
+        f"Do you want to play again {player}? (yes/no): ").strip().lower()
     if not play_again.startswith('y'):
         print(f"Goodbye! {player}")
-
-    break
+        break
