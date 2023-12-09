5c39ab7b59ad4d28d6b9cc7f4330a90e3ea2edda
Mohamed Abdel Ghany <mufasa1611@gmail.com>
2023-12-09 19:35:29 +0000

    add colors

--- run.py
+++ run.py
@@ -1,45 +1,51 @@
-# Import Required Module
-from termcolor import colored
 import random
 import copy
+from termcolor import colored
 
 # Define the Initial Board
 Initial_board = [[1, 2, 3, 4, 5, 6, 7],
                  [1, 2, 3, 4, 5],
                  [1, 2, 3]]
 
-#  Get Player Name
+
+# Get Player Name
 def get_player_name():
     while True:
         player = input("Enter your name: ").strip()
-        if player:
+        if player.isalpha():
             return player
+        else:
+            print(colored("Invalid input. Please enter alphabetic characters only.", "red"))
+
 
 # Display the Initial Board
+
 def display_board(board):
     for i, row in enumerate(board, start=1):
         formatted_row = f"row {i}: " + " ".join([colored('X', 'green')for _ in row])
-
         print(formatted_row)
+
 
 # Take Units from a row
 def take_units(row, units):
     if units < 1 or units > len(row):
         print(
-            f"Invalid input. You can take between 1 \
-            and {len(row)} items. Please Try again.")
+            f"Invalid input. You can take between 1"
+            f"and {len(row)} items. Please Try again.")
         return False
 
     row[-units:] = []
     return True
 
-# Calculate remaining units on the board 
+
+# Calculate remaining units on the board
 def units_left(board):
     return sum(map(len, board))
 
+
 # Execute Player Turn
 def player_turn(board, player):
-    print(f"\n{player}'s Turn:")
+    print(colored(f"\n{player}'s ","blue") + f" Turn:")
     display_board(board)
 
     # Select a row
@@ -48,40 +54,42 @@ def player_turn(board, player):
     while pick_row is None or pick_row < 1 or \
             pick_row > 3 or len(board[pick_row - 1]) == 0:
 
-            try:
-                pick_row = int(input("Choose a row (1, 2, or 3): "))
-                if pick_row < 1 or pick_row > 3:
-                    print("Invalid row choice. Please enter 1, 2, or 3.")
-                elif len(board[pick_row - 1]) == 0:
-                    print("That row is empty. Choose another row.")
-                else:
-                    selected_row = board[pick_row - 1]
-            except ValueError:
-                print("Invalid input. Please enter a valid number.")
+        try:
+            pick_row = int(input(colored("Choose a row (1, 2, or 3): ", "yellow")))
+            if pick_row < 1 or pick_row > 3:
+                print(colored("Invalid row choice. Please enter 1, 2, or 3.", "red"))
+            elif len(board[pick_row - 1]) == 0:
+                print(colored("That row is empty. Choose another row.", "red"))
+            else:
+                selected_row = board[pick_row - 1]
+        except ValueError:
+            print(colored("Invalid input. Please enter a valid number.", "red"))
 
     # Select units
     units = None
-    while units is None or units < 1 or units > len(selected_row) or units >= units_left(board):
+    while units is None or units < 1 or units > len(selected_row) \
+            or units >= units_left(board):
         try:
-            units = int(input(f"How many units to take from row {pick_row}? "))
-            if units is None or units < 1 or units > len(selected_row) or \
-                    units >= units_left(board):
+            units = int(input(colored(f"How many units to take from row {pick_row}? ", "yellow")))
+            if units is None or units < 1 or units > len(selected_row) \
+                    or units >= units_left(board):
                 print(
-                    f"Invalid input. Please enter a number between 1 and ",
-                    f"{min(len(selected_row), units_left(board) - 1)}."
+                    colored(f"Invalid input. Please enter a number between 1 and "
+                            f"{min(len(selected_row), units_left(board) - 1)}.", "red")
                 )
         except ValueError:
-            print("Invalid input. Please enter a valid number.")
+            print(colored("Invalid input. Please enter a valid number.", "red"))
 
     take_units(selected_row, units)
 
-    # Computer turn
+
+# Computer turn
 def computer_turn(board):
-    print("\nComputer's Turn:")
+    print(colored("\nComputer's","magenta") + f" Turn:")
 
     available_rows = [i for i, row in enumerate(board) if len(row) > 0]
     if not available_rows:
-        print("Computer wins!")
+        print(colored("Computer wins!", "red"))
         return
 
     pick_row = random.choice(available_rows)
@@ -89,7 +97,8 @@ def computer_turn(board):
     max_units = min(len(selected_row), units_left(board) - 1)
     units = random.randint(1, max_units)
     take_units(selected_row, units)
-    print(f"Computer took {units} units from row {pick_row + 1}.")
+    print(colored(f"Computer took {units} units from row {pick_row + 1}.", "yellow"))
+
 
 # Game Loop
 def play_game(player):
@@ -100,34 +109,33 @@ def play_game(player):
         display_board(current_board)
 
         if sum(map(len, current_board)) == 1:
-            print(f"{player} wins!")
+            print(colored(f"{player} ","green") + "wins!")
             break
 
         computer_turn(current_board)
 
         if sum(map(len, current_board)) == 1:
             display_board(current_board)
-            print("Computer wins!")
+            print(colored("Computer", "red") + " wins!")
             break
 
 
 # Game intro
-print("\nWelcome to the Game of Rows!")
-print("In this game, you have three rows of tokens (represented by numbers).")
+print(colored("\nWelcome to the Game of Rows!\n", "cyan"))
+print('In this game, you have three rows of tokens represented by "X".')
 print("Each row has a different number of tokens.")
-print("On your turn, you can choose a row and \
-take a certain number of tokens from it.")
+print("On your turn, you can choose a row and take a certain number of tokens from it.")
 print("The goal is to leave the last unit for your opponent.\n")
-print("Let's get started!\n")
+print(colored("Let's get started!\n", "cyan"))
 
 # Game start
-player = get_player_name()
+players = get_player_name()
 while True:
-    play_game(player)
+    play_game(players)
 
-# Retry or quit option
-    play_again = input(
-        f"Do you want to play again {player}? (yes/no): ").strip().lower()
+    # Retry or quit option
+    play_again = input(f"Do you want to play again {players}? (yes/no): ").strip().lower()
     if not play_again.startswith('y'):
-        print(f"Goodbye .. see you soon! {player}")
+        print(colored(f"Goodbye!","cyan")+colored(f" {players}", "blue"))
         break
+        
